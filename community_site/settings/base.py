ae8d5c48638c547238f1f5831b76cb19dfe62794
Sashi445 <sashidhar652@gmail.com>
2021-08-20 09:10:02 +0530

    COMMIT MESSAGE!

--- community_site/settings/base.py
+++ community_site/settings/base.py
@@ -90,12 +90,12 @@ WSGI_APPLICATION = 'community_site.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # db_from_env = dj_database_url.config(conn_max_age=600)
 # DATABASES['default'].update(db_from_env)
