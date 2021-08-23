ebd09ac70459b3bfbec5d2bdb2eb3c5104e9508a
Sashi445 <sashidhar652@gmail.com>
2021-08-23 13:34:14 +0530

    LOT OF CHANGES HAVE BEEN DONE!

--- web_site/models.py
+++ web_site/models.py
@@ -39,8 +39,8 @@ class Category(models.Model):
 
 class Post(models.Model):
     post_type = models.CharField(choices=POST_CHOICES, max_length=10)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    posted_on = models.DateTimeField(auto_now_add=True, default=None)
 
 class Blog(models.Model):
     post_ref = models.OneToOneField(Post, on_delete=models.CASCADE)
